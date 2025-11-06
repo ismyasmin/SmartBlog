56afd92dea79528a8e637713cc6ac12bcbe42357
Yasmin <yasminxismail@gmail.com>
2025-11-06 21:40:39 +0000

    Create models folder inside server folder, used for creating objects for the blog data. Retrieve blog data, do pagination.

--- server/routes/main.js
+++ server/routes/main.js
@@ -1,20 +1,43 @@
 const express = require ('express');
 const router = express.Router();
+const Post = require('../models/Post'); // use thhis model to insert and retrieve data
 
 
 // routes
 
-router.get('', (req,res) => {
-    const locals = {
-        title: "NodeJs Blog",
-        description: "mern ai blog"
+router.get('', async(req,res) => {
+    try{
+        const locals = {
+            title: "NodeJs Blog",
+            description: "ai blog"
+        }
+
+        let perPage = 5;
+        let page = req.query.page || 1; // if there's no page query, set default page query is one and get the first 10 blog plots
+
+        const data = await Post.aggregate([ { $sort: { createdAt: -1}}]) // oldest at the top
+        .skip(perPage * page - perPage)
+        .limit(perPage)
+        .exec(); // execute aggregated pipeline 
+
+        const count = await Post.countDocuments({});
+        const nextPage = parseInt(page) + 1;
+        const hasNextPage = nextPage <= Math.ceil(count / perPage);
+
+        res.render('index', { 
+            locals,
+            data,
+            current: page,
+            nextPage: hasNextPage ? nextPage : null} );
+
+    } catch(error) {
+        console.log(error.message);
+
     }
-
-    res.render('index', { locals } );
 });
 
-router.get('/about', (req,res) => {
-    res.render('about');
-});
+
+
+
 
 module.exports = router;
